07c501b8b95614f627a158e0b1cb781d954644c6
Abdullah Tahir <m.abbytahir@gmail.com>
2024-12-02 15:28:13 +1030

    preparing for day2

---
+++ day2/main.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <queue>
+#include <unordered_map>
+
+
+// partA
+long long solveA() {
+
+}
+
+// partB
+long long solveB() {
+
+}
+
+
+int main() {
+
+    // take in input
+    std::ifstream input("text.txt");
+
+    // check whether the file has been opened
+    if (!input.is_open()) return 1;
+
+    // obtain each line of the input text
+    std::string line;
+    while (std::getline(input, line)) {
+
+
+    }
+
+    return 0;
+
+}
+
+
+
+
